ea635215db130c1b70877d04e04cdd0c5010629c
RomanAndr <andrroman07@gmail.com>
2024-08-31 14:01:42 +0300

    Version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "me.RomanAndr"
-version = "1.0"
+version = "1.3"
 
 repositories {
     mavenCentral()
@@ -30,7 +30,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("231")
-        untilBuild.set("241.*")
+        untilBuild.set("242.*")
     }
 
     signPlugin {
